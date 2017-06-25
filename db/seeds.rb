7ef44b4b1335336e733bac997c369aa2cdeb565a
Meaghan Conroy <mconroy734@gmail.com>
2017-06-25 12:39:20 -0400

    set up restclient for marvel api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'uri'
+require 'rest-client'
+require 'dotenv-rails'
+require 'digest/md5'
+Comic.destroy_all
+
+public_key = ENV['MARVEL_API_PUBLIC_KEY']
+private_key = ENV['MARVEL_API_PRIVATE_KEY']
+ts = Time.new
+
+hash = Digest::MD5.hexdigest(ts.to_s+private_key+public_key)
+
+response = RestClient.get "http://gateway.marvel.com/v1//public/comics?ts=#{ts}&apikey=#{public_key}&hash=#{hash}"
+
+comics = JSON.parse(response)
+comics["series"].each do |comic|
+  Comic.create!(
+    publisher: "Marvel",
+    title: comic["title"],
+    creators:
+      comic["creators"]["items"].each do |creator|
+        name = creator["name"]
+        role = creator["role"]
+        "#{name}, #{role}"
+      end,
+    characters: comic["characters"]["items"].each do |character|
+      character["name"]
+    end,
+    photo: comic["images"]["path"] + "." + comic["images"]["extension"]
+  )
+end
