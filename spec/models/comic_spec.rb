0c24a872180248767e05fe834506f5a9a5a0bf19
Meaghan Conroy <mconroy734@gmail.com>
2017-06-26 16:21:53 -0400

    added model tests for comics

---
+++ spec/models/comic_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Comic, type: :model do
+  it { should have_valid(:publisher).when('Marvel', 'DC') }
+  it { should_not have_valid(:publisher).when(nil,'') }
+
+  it { should have_valid(:title).when('Captain Marvel', 'Squirrel Girl') }
+  it { should_not have_valid(:title).when(nil,'') }
+end
