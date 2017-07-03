935f397b949db549c383ff7adb538d43bb3bbd94
Meaghan Conroy <mconroy734@gmail.com>
2017-07-03 16:18:46 -0400

    profile photo upload working

--- spec/features/users/user_signs_up_spec.rb
+++ spec/features/users/user_signs_up_spec.rb
@@ -11,10 +11,12 @@ feature "User signs up" do
     fill_in 'Email', with: "hermionegranger@launchacademy.com"
     fill_in 'Password', with: "testtest"
     fill_in 'Password Confirmation', with: "testtest"
+    attach_file 'Avatar', "#{Rails.root}/spec/support/photo.jpg"
 
     click_button 'Sign Up'
 
     expect(page).to have_content('Welcome! You have signed up successfully.')
     expect(page).to have_content('Sign Out')
+    expect(page).to have_css("img[src*='photo.jpg']")
   end
 end
