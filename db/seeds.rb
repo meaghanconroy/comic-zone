77be176eb484d003ceb50b8b077516e54a4341e6
Meaghan Conroy <mconroy734@gmail.com>
2017-07-03 13:24:32 -0400

    db config

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,26 @@ Comic.destroy_all
 public_key = ENV['MARVEL_API_PUBLIC_KEY']
 private_key = ENV['MARVEL_API_PRIVATE_KEY']
 ts = Time.new
+ts2 = Time.new
+ts3 = Time.new
+ts4 = Time.new
+ts5 = Time.new
+ts6 = Time.new
 timestamp = ts.to_s
+timestamp_two = ts2.to_s
+timestamp_three = ts3.to_s
+timestamp_four = ts4.to_s
+timestamp_five = ts5.to_s
+timestamp_six = ts6.to_s
+
 hash = Digest::MD5.hexdigest("#{timestamp}"+"#{private_key}"+"#{public_key}")
 
 first_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50"
-second_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=50"
-third_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=100"
-fourth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=150"
-fifth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=200"
-sixth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=250"
+second_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp_two}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=50"
+third_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp_three}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=100"
+fourth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp_four}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=150"
+fifth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp_five}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=200"
+sixth_response = RestClient.get "http://gateway.marvel.com/v1//public/series?ts=#{timestamp_six}&apikey=#{public_key}&hash=#{hash}&seriesType=ongoing&limit=50&offset=250"
 
 first_comics = JSON.parse(first_response)
 first_data = first_comics["data"]["results"]
