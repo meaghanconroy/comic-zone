7deccacca6568e54cac6202d27fb2094163551df
Meaghan Conroy <mconroy734@gmail.com>
2017-07-04 18:09:48 -0400

    heroku stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,19 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'comiczone.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
+  config.action_mailer.default_url_options = { :host => 'comiczone.herokuapp.com' }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
