6c6689e7538a5b2659087e3170a792272582d9ce
Meaghan Conroy <mconroy734@gmail.com>
2017-07-05 07:56:51 -0400

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,3 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-
-if (Modernizr.testAllProps('gridRowGap') === false) {
-  $('body').append("<div class='no-grid-warning'>Your browser does not support CSS-Grid - this demo requires CSS-grid to function properly</div>");
-}
-Contact GitHub API Training Shop Blog About
